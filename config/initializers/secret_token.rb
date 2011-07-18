e8e0407b380195fa8053d2d2b66eaa7003601778
Glen E. Ivey <gleneivey@wontology.org>
2011-07-17 23:10:31 -0700

    result of 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DraftOutloud::Application.config.secret_token = 'a287e6e4a2f81b1f754957db68d974df592cde9141b2f94278b67d7437c5500f7160bf376d982bec725d9f73de7d13a2e63e5627d4e6c38778aa21a31ca02b10'
