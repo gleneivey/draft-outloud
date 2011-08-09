4f200a4e97632191563862adbd4de3d212c87e11
Glen E. Ivey <gleneivey@wontology.org>
2011-08-08 21:14:33 -0700

    update cap deploy to not daemonize Mongrel

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ end
 
 def a2_mongrel_start
   run "cd #{current_path} && " +
-      "bundle exec mongrel_rails start -d -p #{a2_port} -e production -P log/mongrel.pid < /dev/null >& /dev/null"
+      "bundle exec rails server -p #{a2_port} -e production -P log/mongrel.pid < /dev/null >& /dev/null &"
 end
 
 def a2_mongrel_stop
