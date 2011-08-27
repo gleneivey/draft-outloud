9ddc3a5ada22f63d0e802d7cf707563b139c6053
Glen E. Ivey <gleneivey@wontology.org>
2011-08-26 22:38:40 -0700

    [#16822693] the rest of the DocBook toolchain files, meant to be in earlier

---
+++ toolchain-docbook/doit.sh
@@ -0,0 +1,30 @@
+### This is a trivial script for processing a DocBook book into PDF, I use
+### it to test the content of the toolchain directory.
+
+
+echo 'validate against schema'
+java -jar bin/jing.jar -C schema/catalog.xml schema/docbook.rng $1
+
+
+# don't need olink processing yet
+
+# XML -> FO -> PDF
+echo 'XML to FO'
+java -cp bin:bin/saxon.jar:bin/resolver.jar:bin/xercesImpl.jar -Xmx1G \
+      -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl \
+      -Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl \
+      -Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration \
+    com.icl.saxon.StyleSheet \
+      -x org.apache.xml.resolver.tools.ResolvingXMLReader \
+      -y org.apache.xml.resolver.tools.ResolvingXMLReader \
+      -r org.apache.xml.resolver.tools.CatalogResolver \
+    -o book.fo \
+    $1 \
+      style/pdf.xsl \
+      target.database.document=book-site-pdf.xml \
+      current.docid=book
+
+echo 'FO to PDF'
+java -Xmx1G -cp bin/fop.jar:bin/fop-hyph.jar:bin/avalon-framework.jar:bin/batik-all.jar:bin/commons-io.jar:bin/commons-logging.jar:bin/xmlgraphics-commons.jar \
+    org.apache.fop.cli.Main \
+      -q -fo book.fo -pdf book.pdf
