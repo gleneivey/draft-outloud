4c4db9cd799777f6fe105d8b40c16c8eecfe082e
Glen E. Ivey <gleneivey@wontology.org>
2011-09-02 20:47:51 -0700

    [finishes #16822703] add HTML download link to home page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -6,10 +6,14 @@ describe "home/index.html.erb" do
     assign :title, title
     subtitle = "For Dummies"
     assign :subtitle, subtitle
+    book_file_name = "this-book"
+    assign :book_file_name, book_file_name
 
     render
 
     rendered.should have_xpath "//div[@class='title'][.='#{title}']"
     rendered.should have_xpath "//div[@class='subtitle'][.='#{subtitle}']"
+    rendered.should have_xpath "//a[@href='/#{book_file_name}.pdf'][.='Download PDF']"
+    rendered.should have_xpath "//a[@href='/#{book_file_name}.html'][.='Download HTML']"
   end
 end
