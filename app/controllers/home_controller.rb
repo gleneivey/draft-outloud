5032962b6f0395068cce65eea33880d7ab5d587c
Glen E. Ivey <gleneivey@wontology.org>
2011-09-06 20:30:49 -0700

    Don't 500 on home page if no successful content updates yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,15 +10,19 @@ class HomeController < ApplicationController
     @book_file_name = File.basename(path, ".xml")
 
     @toc_entry_hashes = []
-    ContentUpdate.where('successful = 1').last.toc_entries.each do |entry|
-      text_sections = entry.entry_text.split(NONBREAKING_SPACE)
-      number = text_sections.length == 1 ? nil :
-          text_sections[0..-2].join(' ')
-      @toc_entry_hashes << {
-          :number => number,
-          :text => text_sections.last,
-          :level => entry.indent_level
-      }
+    toc_entries = []
+    content_entries = ContentUpdate.where('successful = 1').last
+    if content_entries && toc_entries = content_update.toc_entries
+      toc_entries.each do |entry|
+        text_sections = entry.entry_text.split(NONBREAKING_SPACE)
+        number = text_sections.length == 1 ? nil :
+            text_sections[0..-2].join(' ')
+        @toc_entry_hashes << {
+            :number => number,
+            :text => text_sections.last,
+            :level => entry.indent_level
+        }
+      end
     end
   end
 end
